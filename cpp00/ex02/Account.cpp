57eecc89956b70c84e309d1eb4d79c4cbb96f2d6
Bombatomica_64 <qualcosa2001@gmail.com>
2024-04-03 12:01:24 +0200

    Update Account class with new member variables and methods

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,16 +6,25 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 10:41:26 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/04/03 10:50:44 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/04/03 12:00:25 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
 Account::Account( int initial_deposit )
 {
-	
+	this->_totalAmount += initial_deposit;
+}
+
+Account::~Account( void )
+{
 }
 
 int	Account::getNbAccounts( void ) {
@@ -36,4 +45,35 @@ int	Account::getNbWithdrawals( void ) {
 
 void	Account::displayAccountsInfos( void ) {
 	std::cout << "Accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::makeDeposit( int deposit ) {
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+	if (withdrawal > _totalAmount)
+		return false;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	return true;
+}
+
+
+int		Account::checkAmount( void ) const {
+	return _amount;
+}
+
+void	Account::displayStatus( void ) const {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp( void ) {
+	time_t		t = time(0);
+
+	char		buff[80];
+	strftime(buff, 80, "%Y%m%d_%H%M%S", localtime(&t));
+	std::cout << "[" << buff << "] ";
 }
