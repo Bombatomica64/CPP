1c2131633152714b5078029846ecde680f1ca833
Bombatomica_64 <qualcosa2001@gmail.com>
2024-04-04 15:18:43 +0200

    Update megaphone.cpp and Makefile

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:00:09 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/04/04 13:00:28 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:15:12 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,19 @@
 #include <iostream>
 #include "HumanA.hpp"
 #include "HumanB.hpp"
+
+class Weapon
+{
+
+	public:
+		Weapon(std::string type);
+		Weapon(const Weapon &other);
+		~Weapon();
+		std::string getType();
+		void setType(std::string type);
+	
+	private:
+		std::string type;
+};
 
 #endif
