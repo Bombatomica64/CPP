c55e2053f9fdf9c1de4b50dd7ff9950f4fcc2f83
Bombatomica_64 <qualcosa2001@gmail.com>
2024-04-24 17:41:50 +0200

    new cpp

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 17:39:51 by lmicheli          #+#    #+#             */
+/*   Updated: 2024/04/24 17:41:42 by lmicheli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+Class ClapTrap
+{
+	private:
+		std::string Name;
+		 Hitpoints;
+};
+
+#endif
